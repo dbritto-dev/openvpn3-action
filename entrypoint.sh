51af04989825e485bd286d353e2d5b021cebe786
dbritto-dev <dbritto.dev@gmail.com>
2022-04-02 10:59:21 -0500

    Update entrypoint scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,13 +13,13 @@ VPN_CLIENT_AUTOLOAD_PATH=$VPN_CLIENT_DIR/config.autoload
 mkdir -m700 -p $VPN_CLIENT_DIR
 if [ -z "$VPN_AUTOLOAD_CONFIG" ]; then
   echo "$VPN_CLIENT_CONFIG" | base64 --decode >> $VPN_CLIENT_CONFIG_PATH
-  sudo openvpn3 session-start --config $VPN_CLIENT_CONFIG_PATH
+  openvpn3 session-start --config $VPN_CLIENT_CONFIG_PATH
 else
   echo "$VPN_CLIENT_CONFIG" | base64 --decode >> $VPN_CLIENT_CONFIG_PATH
   echo "$VPN_AUTOLOAD_CONFIG" | base64 --decode >> VPN_CLIENT_AUTOLOAD_PATH
-  sudo openvpn3-autoload --directory $VPN_CLIENT_DIR
+  openvpn3-autoload --directory $VPN_CLIENT_DIR
 fi
-while [ -z "$(sudo openvpn3 sessions-list | grep -io 'client connected')" ]; do
+while [ -z "$(openvpn3 sessions-list | grep -io 'client connected')" ]; do
   sleep 0.1;
 done
 
--- pre-entrypoint.sh
+++ pre-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo apt-get install -y apt-transport-https
-sudo wget https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub
-sudo apt-key add openvpn-repo-pkg-key.pub
-sudo wget -O /etc/apt/sources.list.d/openvpn3.list https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-$DISTRO.list
-sudo apt-get update -y && sudo apt-get install -y openvpn3
+apt-get install -y apt-transport-https
+wget https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub
+apt-key add openvpn-repo-pkg-key.pub
+wget -O /etc/apt/sources.list.d/openvpn3.list https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-$DISTRO.list
+apt-get update -y && apt-get install -y openvpn3
