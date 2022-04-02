7cd2a70492d0232c489b4143e0bc6307b339836e
dbritto-dev <dbritto.dev@gmail.com>
2022-04-02 18:25:51 -0500

    Testing

--- pre-entrypoint.sh
+++ pre-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-apt-get install -y apt-transport-https curl
-curl https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub -s -o openvpn-repo-pkg-key.pub
+apt-get install -y apt-transport-https gnupg2 wget
+wget https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub
 apt-key add openvpn-repo-pkg-key.pub
-curl https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-$DISTRO.list -s -o /etc/apt/sources.list.d/openvpn3.list
+wget -O /etc/apt/sources.list.d/openvpn3.list https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-$DISTRO.list
 apt-get update -y && apt-get install -y openvpn3
