fbfd01c4f180cfc3c480952781d713a399d77e73
dbritto-dev <dbritto.dev@gmail.com>
2022-04-02 18:24:01 -0500

    Testing

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM ubuntu:focal
 
-ENV PATH=/:$PATH
-
 COPY pre-entrypoint.sh /pre-entrypoint.sh
 COPY entrypoint.sh /entrypoint.sh
 COPY post-entrypoint.sh /post-entrypoint.sh
--- pre-entrypoint.sh
+++ pre-entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-apt-get install -y apt-transport-https
-apt-get install -y wget
-wget https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub
+apt-get install -y apt-transport-https curl
+curl https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub -s -o openvpn-repo-pkg-key.pub
 apt-key add openvpn-repo-pkg-key.pub
-wget -O /etc/apt/sources.list.d/openvpn3.list https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-$DISTRO.list
+curl https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-$DISTRO.list -s -o /etc/apt/sources.list.d/openvpn3.list
 apt-get update -y && apt-get install -y openvpn3
